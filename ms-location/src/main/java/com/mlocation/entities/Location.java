d6da198b2ec652009375f42fb25e388096ccce46
Guillaume VITTOZ <vittoz.guillaume@live.fr>
2021-01-27 09:00:59 +0100

    location maj

--- ms-location/src/main/java/com/mlocation/entities/Location.java
+++ ms-location/src/main/java/com/mlocation/entities/Location.java
@@ -5,12 +5,16 @@ import java.io.Serializable;
 //import java.util.List;
 import javax.persistence.Entity;
 //import javax.persistence.OneToMany;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Location implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idLocation;
 	private String locationCode;
 	private String locationName;
